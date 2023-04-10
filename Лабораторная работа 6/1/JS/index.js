3266348a857cd150ba475e995ab11545ffbcaaec
thefidden <90830639+thefidden@users.noreply.github.com>
2023-04-10 19:38:00 +0300

    Delete Лабораторная работа 6 directory

--- Лабораторная работа 6/1/JS/index.js
+++
@@ -1,6 +0,0 @@
-let stack = new Stack()
-
-for (let i = 0; i < 5; ++i)
-    stack.insert(getRandomInt(0, 100))
-
-
--- Лабораторная работа 6/2/JS/index.js
+++
@@ -1,6 +0,0 @@
-let queue = new Queue()
-
-for (let i = 0; i < 5; ++i)
-    queue.insert(getRandomInt(0, 100))
-
-
